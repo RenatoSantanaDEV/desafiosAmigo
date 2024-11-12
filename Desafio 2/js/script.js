9e9e43ee19fda449af83437951e45b7a17090e67
Renato Almeida <Renato-Almeida@DEV-RECIFE-0137.local>
2024-11-12 17:11:35 -0300

    Sinal Funcionando

--- Desafio 2/js/script.js
+++ Desafio 2/js/script.js
@@ -1,95 +1,120 @@
-// Ao entrar na tela o semáforo já inicia VERDE (aberto)
-// O semáforo fecha a cada 10 segundos
-// O semáforo deve permanecer 3 segundos no AMARELO
-// O semáforo deve permanecer por 15 segundos fechado
-// Crie um botão para simular a ação de um pedestre solicitando para fechar o sinal e atravessar a pista.
-//ok
-// Ao clicar no botão para FECHAR se o semáforo estiver verde, ele deve aguardar 3 segundos até fechar. 
-// Se ao clicar para fechar estiver no amarelo continua o processo de fechamento.
-// Informe ao lado do semáforo o tempo do sinal atual em segundos (menos para sinal amarelo)
-function Timer(callback, delay) {
-    return setTimeout(callback, delay);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    inicia();
+});
+
 const verde = document.getElementById('green');
 const amarelo = document.getElementById('yellow');
 const vermelho = document.getElementById('red');
-const divBotao = document.querySelector('.wrapper');
 const botao = document.getElementById('botao');
-const relogio = document.getElementById('relogio');
-const leitorVerde = document.getElementById('leitor-verde');
-const leitorVermelho = document.getElementById('leitor-vermelho');
-const contadorDiv = document.getElementById('contador');
-const contadores = document.querySelector('.contadores');
+const relogio = document.querySelector('.contadores');
+const cronometro = document.getElementById('relogio');
+const passagem = document.querySelector('.passagem');
+const verdePedestre = document.getElementById('pedestrian-green');
+const vermelhoPedestre = document.getElementById('pedestrian-red');
 
-let flag = false;
-let contador = 60;
-let contadorVerde = 10;
+let isPedestrianRequest = false;
+let timerIntervalVerde, timerIntervalAmarelo, timerIntervalVermelho;
+let time = 60;
 
-relogio.innerHTML = `FECHAR`;
+cronometro.innerHTML = `FECHAR`;
 
-function intervalo (contador){
-    const intervaloGeral = setInterval(function(){
-        if (contador >= 0) {
-            contadores.innerHTML = `00:${String(contador).padStart(2, '0')}`;
-            contador--;
-        } else if (contador <= 0) {
-            clearInterval(intervaloGeral);
-            contadores.innerHTML = `00:00`;
+function atualizaRelogio(tempo) {
+    tempoPed = 13;
+    relogio.innerHTML = `00:${String(tempo).padStart(2, '0')}`;
+    
+}
+
+function atualizaCronometro() {
+    botao.disabled = true;
+    cronometro.innerHTML = `00:${String(time).padStart(2, '0')}`;
+
+    const countdownInterval = setInterval(() => {
+        time--;
+        cronometro.innerHTML = `00:${String(time).padStart(2, '0')}`;
+
+        if (time === 0) {
+            clearInterval(countdownInterval);
+            botao.disabled = false;
+            cronometro.innerHTML = `FECHAR`;
+            time = 60;
         }
     }, 1000);
 }
 
-
-
-if(flag === false){
-    botao.addEventListener('click', function(){
-        // contadorVerde = 3;
-        flag = true;
-        botao.disabled = true;
-
-        inicia();
-        setTimeout(() => {
-            botao.style.backgroundColor = 'red';
-            flag = false;
-            botao.disabled = false;
-            contador = 10;
-        }, 60000);
-        const intervalo = setInterval(function(){
-            if (contador > 0) {
-                relogio.innerHTML = `00:${String(contador).padStart(2, '0')}`;
-                contador--;
-            } else if (contador <= 0) {
-                clearInterval(intervalo);
-                relogio.innerHTML = `FECHAR`;
-            }
-        }, 1000);
-    })
-}
 function inicia() {
+    mudaParaVerde(10);
+    botao.addEventListener('click', () => {
+        if (!isPedestrianRequest) {
+            isPedestrianRequest = true;
+            if (verde.style.backgroundColor === 'green') {
+                atualizaCronometro();
+                clearInterval(timerInterval);
+                tempoVerde = 2;
+                mudaParaVerde(tempoVerde);
+                setTimeout(() => mudaParaAmarelo(4), 3000);
+            }
+        }
+    });
+}
+
+function mudaParaVerde(tempoVerde) {
     verde.style.backgroundColor = 'green';
-    botao.disabled = flag;
-    Timer(function() {
-        botao.disabled = true;
-        verde.style.backgroundColor = 'rgba(0, 255, 0, 0.255)'; 
-        amarelo.style.backgroundColor = 'yellow'; 
-    }, contadorVerde*1000);
-    Timer(function() {
-        botao.disabled = true;
-        verde.style.backgroundColor = 'rgba(0, 255, 0, 0.255)'; 
-        amarelo.style.backgroundColor = 'yellow'; 
-    }, 3000);
+    amarelo.style.backgroundColor = 'rgba(255, 255, 0, 0.2)';
+    vermelho.style.backgroundColor = 'rgba(255, 0, 0, 0.2)';
+    verdePedestre.style.backgroundColor = 'rgba(0, 255, 0, 0.2)';
+    vermelhoPedestre.style.backgroundColor = 'red';
+    passagem.innerHTML = 'PARE';
 
-    Timer(function() {
-        contadores.style.color = 'red';
-        botao.style.backgroundColor = 'red';
-        botao.disabled = true;
-        amarelo.style.backgroundColor = 'rgba(255, 255, 0, 0.225)';
-        vermelho.style.backgroundColor = 'red';
-        intervalo(14);
+    atualizaRelogio(tempoVerde);
 
-    }, 13000);
-    Timer(function() {
-        vermelho.style.backgroundColor = 'rgba(255, 0, 0, 0.255)';
-        inicia(); 
-    }, 28000);
+    timerIntervalVerde = setInterval(() => {
+        tempoVerde--;
+        atualizaRelogio(tempoVerde);
+        if (tempoVerde === 0) {
+            clearInterval(timerIntervalVerde);
+            mudaParaAmarelo(4);
+        }
+    }, 1000);
+}
+
+function mudaParaAmarelo(tempoAmarelo) {
+    verde.style.backgroundColor = 'rgba(0, 255, 0, 0.2)';
+    amarelo.style.backgroundColor = 'yellow';
+    verdePedestre.style.backgroundColor = 'rgba(0, 255, 0, 0.2)';
+    vermelhoPedestre.style.backgroundColor = 'red';
+    passagem.innerHTML = 'PARE';
+
+    atualizaRelogio(tempoAmarelo);
+
+    timerIntervalAmarelo = setInterval(() => {
+        tempoAmarelo--;
+        atualizaRelogio(tempoAmarelo);
+        if (tempoAmarelo === 0) {
+            clearInterval(timerIntervalAmarelo);
+            mudaParaVermelho(15);
+        }
+    }, 1000);
+}
+
+function mudaParaVermelho(tempoVermelho) {
+    amarelo.style.backgroundColor = 'rgba(255, 255, 0, 0.2)';
+    vermelho.style.backgroundColor = 'red';
+    vermelhoPedestre.style.backgroundColor = 'rgba(255, 0, 0, 0.255)'
+    verdePedestre.style.backgroundColor = 'green';
+    passagem.innerHTML = 'SIGA';
+    passagem.style.color = 'green';
+
+    atualizaRelogio(tempoVermelho);
+
+    timerIntervalVermelho = setInterval(() => {
+        tempoVermelho--;
+        atualizaRelogio(tempoVermelho);
+        if (tempoVermelho === 0) {
+            clearInterval(timerIntervalVermelho);
+            isPedestrianRequest = false;
+            tempoVerde = 10;
+            mudaParaVerde(tempoVerde);
+            passagem.style.color = 'red';
+        }
+    }, 1000);
 }
