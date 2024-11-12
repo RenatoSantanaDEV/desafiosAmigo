fe0e3e1b7b046bd670110fcf1ca7e0d65baf1827
Renato Almeida <Renato-Almeida@DEV-RECIFE-0137.local>
2024-11-12 19:00:16 -0300

    Desafio 1

--- Desafio 1/assets/js/script.js
+++ Desafio 1/assets/js/script.js
@@ -1,38 +1,45 @@
-function criahoraDosSegundo(segundos){
-    const data = new Date(segundos * 1000);
-    return data.toLocaleTimeString('pt-br', { hour12 : false, timeZone: 'UTC' });
-}
-const relogio = document.querySelector('.relogio');
-const iniciado = document.querySelector('.inicar');
-const pausado = document.querySelector('.pausar');
-const zerado = document.querySelector('.zerar');
-const estilosBotao = getComputedStyle(document.body);
-let segundos =0;
-let timer;
+document.getElementById('cpf').addEventListener('input', function () {
+    let cpf = this.value;
+    cpf = cpf.replace(/\D/g, '');
+    if (cpf.length > 6) {
+        cpf = cpf.replace(/^(\d{3})(\d{3})(\d{3})(\d{0,2})$/, '$1.$2.$3-$4');
+    } else if (cpf.length > 3) {
+        cpf = cpf.replace(/^(\d{3})(\d{3})(\d{0,3})$/, '$1.$2.$3');
+    } else {
+        cpf = cpf.replace(/^(\d{3})$/, '$1.');
+    }
+    this.value = cpf.slice(0, 14);
+});
 
-function mostraHora(){
-    timer = setInterval( function() { segundos++; relogio.innerHTML = criahoraDosSegundo(segundos); }, 1000 );
-}
-function inicia(){
-    iniciado.style.background = "green";
-    pausado.style.background = null;
-    zerado.style.background = null;
-    relogio.classList.remove('pausado-cor');
-    mostraHora();
-}
-function pausa(){
-    relogio.classList.add('pausado-cor');
-    pausado.style.background = "green";
-    iniciado.style.background = null;
-    zerado.style.background = null;
-    
-    clearInterval(timer);
-}
-function zera(){
-    iniciado.style.background = null;
-    zerado.style.background = "green";
-    pausado.style.background = null;
-    clearInterval(timer);
-    relogio.innerHTML = "00:00:00";
-    segundos = 0;
-}
+document.getElementById('nascimento').addEventListener('input', function () {
+    let nascimento = this.value;
+    nascimento = nascimento.replace(/\D/g, '');
+    if (nascimento.length > 4) {
+        nascimento = nascimento.replace(/^(\d{2})(\d{2})(\d{0,4})$/, '$1/$2/$3');
+    } else if (nascimento.length > 2) {
+        nascimento = nascimento.replace(/^(\d{2})(\d{0,2})$/, '$1/$2');
+    }
+    this.value = nascimento.slice(0, 10);
+});
+
+document.getElementById('telefone').addEventListener('input', function(){
+    let telefone = this.value;
+    telefone = telefone.replace(/\D/g, '');
+    if (telefone.length > 7) {
+        telefone = telefone.replace(/^(\d{2})(\d{5})(\d{0,4})$/, '($1) $2-$3');
+    }
+    else if (telefone.length > 2) {
+        telefone = telefone.replace(/^(\d{2})(\d{0,5})$/, '($1) $2');
+    }
+    this.value = telefone.slice(0,15)
+});
+
+document.getElementById('pessoa').addEventListener('input', function () {
+    const nome = this.value;
+    this.value = nome.replace(/[^a-zA-ZÀ-ÖØ-öø-ÿ\s]/g, '');
+    if (nome.length < 3) {
+        console.log("O nome deve ter pelo menos 3 caracteres.");
+    } else if (nome.length > 50) {
+        console.log("O nome deve ter no máximo 50 caracteres.");
+    }
+});
